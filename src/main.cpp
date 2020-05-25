67f432b8ff9b446279e98a15ffa97b4d9ef89f0e
Joel Tanner <joel@tanner-family.org>
2020-05-24 20:25:53 -0400

    add interfacing functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,52 @@
 #include "main.h"
 
+//Custom Electonics Ports
+int ce_write = 13;
+int ce_read = 14;
+
+//Comm variables
+pros::Serial *ce_port_w = nullptr;
+pros::Serial *ce_port_r = nullptr;
+
+void init_comm(){
+  //Setup the ports
+  ce_port_w = new pros::Serial(ce_write);
+  ce_port_r = new pros::Serial(ce_read);
+}
+
+//flush the comm ports
+void flush(){
+  ce_port_w->flush();
+  pros::delay(100);
+  ce_port_r->flush();
+}
+
+//set the port baud rate
+void setBaud(){
+  ce_port_w->set_baudrate(115200);
+  ce_port_r->set_baudrate(115200);
+}
+
+//read byte from port and print to cout
+void recv_print(){
+  //Read byte command
+  int32_t r = ce_port_r->read_byte();
+  //Check if error
+  if(r == PROS_ERR){
+    std::cout << pros::millis() << " Error read " << r << std::endl;
+  } else {
+    //Print recieved byte
+    std::cout << pros::millis() << " Pass read " << r << std::endl;
+  }
+}
+
+void send_print(char value){
+	//Send the value byte
+  int32_t w = ce_port_w->write_byte(value);
+	std::cout << "Sent Value " << value << std::endl;
+  pros::delay(3);
+}
+
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
